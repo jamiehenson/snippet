34474f490d8a2253dc6126e18fa9af8854f55a0e
Jamie Henson <jhenson47@gmail.com>
2016-02-25 00:39:14 +0000

    CSS minification

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,6 +4,8 @@ var autoprefixer = require('gulp-autoprefixer');
 var coffee = require('gulp-coffee');
 var jade = require('gulp-jade');
 var gutil = require('gulp-util');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
 
 gulp.task('sass', function() {
   gulp.src('./src/css/*.scss')
@@ -24,6 +26,15 @@ gulp.task('jade', function() {
     .pipe(gulp.dest('./'));
 });
 
+gulp.task('minify-css', function() {
+  return gulp.src(['./dist/css/*.css', '!./dist/css/*.min.css'])
+    .pipe(cleanCSS())
+    .pipe(rename({
+      extname: ".min.css"
+    }))
+    .pipe(gulp.dest('./dist/css'));
+});
+
 gulp.task('watch', function() {
   gulp.watch('./src/css/*.scss', ['sass']).on('change', function(event) {
     console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
@@ -32,6 +43,9 @@ gulp.task('watch', function() {
     console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
   });
   gulp.watch('./src/html/*.jade', ['jade']).on('change', function(event) {
+    console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+  });
+  gulp.watch(['./dist/css/*.css', '!./dist/css/*.min.css'], ['minify-css']).on('change', function(event) {
     console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
   });
 });
