8872f30e6d1ae8b5f28148d62cedd6a1e9bf25eb
Jamie Henson <jhenson47@gmail.com>
2016-02-27 21:20:19 +0000

    Make reveal less buggered

--- dist/js/snippets.js
+++ dist/js/snippets.js
@@ -6,19 +6,19 @@ $(document).ready(function() {
   });
   return $('.snippet-expander').click(function() {
     var boxSize, openHeight;
-    if ($('.snippet-expander').hasClass("snippet-reveal")) {
-      if ($('.snippet-expander').hasClass("open-delayed")) {
-        $('.snippet-expander').removeClass("open-delayed");
-      } else {
-        setTimeout((function() {
-          return $('.snippet-expander').addClass("open-delayed");
-        }), 500);
-      }
-    }
     $(this).toggleClass('open');
     $(this).siblings('.snippet-content').toggleClass('open');
     openHeight = $(this).siblings('.snippet-content')[0].scrollHeight + 'px';
     boxSize = $(this).hasClass('open') ? openHeight : '100px';
-    return $(this).siblings('.snippet-content').css('max-height', boxSize);
+    $(this).siblings('.snippet-content').css('max-height', boxSize);
+    if ($('.snippet-expander').hasClass("snippet-reveal")) {
+      if ($('.snippet-expander').hasClass("open-delayed")) {
+        return $('.snippet-expander').removeClass("open-delayed");
+      } else {
+        return setTimeout((function() {
+          return $('.snippet-expander').addClass("open-delayed");
+        }), 500);
+      }
+    }
   });
 });
--- src/js/snippets.coffee
+++ src/js/snippets.coffee
@@ -4,6 +4,13 @@ $(document).ready ->
       $(this).siblings('.snippet-expander').hide()
 
   $('.snippet-expander').click ->
+    $(this).toggleClass 'open'
+    $(this).siblings('.snippet-content').toggleClass 'open'
+
+    openHeight = $(this).siblings('.snippet-content')[0].scrollHeight + 'px'
+    boxSize = if $(this).hasClass('open') then openHeight else '100px'
+    $(this).siblings('.snippet-content').css 'max-height', boxSize
+
     if $('.snippet-expander').hasClass("snippet-reveal")
       if $('.snippet-expander').hasClass("open-delayed")
         $('.snippet-expander').removeClass("open-delayed")
@@ -11,10 +18,3 @@ $(document).ready ->
         setTimeout (->
           $('.snippet-expander').addClass("open-delayed")
         ), 500
-
-    $(this).toggleClass 'open'
-    $(this).siblings('.snippet-content').toggleClass 'open'
-
-    openHeight = $(this).siblings('.snippet-content')[0].scrollHeight + 'px'
-    boxSize = if $(this).hasClass('open') then openHeight else '100px'
-    $(this).siblings('.snippet-content').css 'max-height', boxSize
